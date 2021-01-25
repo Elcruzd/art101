b92e682fe1f3bcc5b9ca6b42e0e267bd87826f3b
Elcruzd <77077807+Elcruzd@users.noreply.github.com>
2021-01-25 10:50:48 -0800

    same

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+// Register user movement, up arrow.
+    // setup leftship
+        // setup rightship
+            // setup key, up arrow
+
+// Randomize asteroids outside of the screen and make them move only horizontally.
+    // create the asteroids
+        // loop the asteroids
+            // If an asteroid collides with the player, reset the location.
+
+// Add score.
+    // create score object
+        // leftScore
+            // rightScore
+                // If the player reaches the top of the screen, add 1 to the score and reset the location.
+                    // Choose a player with a higher score and display it as the winner.
+                        // If timer end time, compare player scores.
+
+//Add timer
+    // create timer object
