d42bb309ae533f1227f3b5ea81c5b62ffc0fe80f
Elcruzd <77077807+Elcruzd@users.noreply.github.com>
2021-01-27 12:47:40 -0800

    lab5 update

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Zhendong Jiang
+* Created: 1/26/2021
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Honda";
+model = "Civic";
+color = "Black";
+year = 2019;
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
