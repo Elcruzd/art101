a24eb64bdaaba3b24b8ff1ebb87650165ce4456d
Elcruzd <77077807+Elcruzd@users.noreply.github.com>
2021-01-31 16:22:33 -0800

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Zhendong Jiang
+* Created: 1/28/2021
+* License: Public Domain
+*/
+
+// Define Variables
+myTransport = ["sedan car", "bus", "bicycle"];
+
+myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Black",
+  year: 2019,
+  age: function() {
+    return 2021 - age;
+  }
+}
+
+// output
+document.writeln("Kinds of transportion I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
