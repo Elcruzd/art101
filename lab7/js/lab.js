58da790df3f06570cf3eb570a766fae07eb21bac
Elcruzd <77077807+Elcruzd@users.noreply.github.com>
2021-02-03 16:47:19 -0800

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,3 +3,23 @@
 * Created: 2/2/2021
 * License: Public Domain
 */
+
+// sortName is a function that takes user input and sorts the letters
+// of their name
+function sortName(){
+  var userName = window.prompt("Which name do you want me to fix?");
+  console.log("userName =", userName);
+  // split string to array
+  var strArray = userName.split('');
+  console.log("strArray =", strArray);
+  // sort the array
+  var arraySort = strArray.sort();
+  console.log("arraySort =", arraySort);
+  // join array back to string
+  var strSort = arraySort.join('');
+  console.log("strSort =", strSort);
+  return strSort;
+}
+
+// Output
+document.writeln("Fixed! Here's your name: ", sortName(), "</br>");
